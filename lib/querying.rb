a5a66752ac120bd9377f0d63b9fccd7ca42305c0
Mars Le <hp.mars.le@gmail.com>
2019-10-26 00:03:52 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,5 +21,4 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(*) FROM characters JOIN character_books ON characters.id = character_books.character_id GROUP BY characters.name ORDER BY COUNT(*) DESC" 
-  # INNER JOIN character_books INNER JOIN books 
 end
